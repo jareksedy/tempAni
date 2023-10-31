db2cdc776116f610aa697e31337c625ad18da00b
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-10-31 20:45:16 +0600

    Finish animations.

--- tempAni/ViewController.swift
+++ tempAni/ViewController.swift
@@ -45,6 +45,18 @@ class ViewController: UIViewController {
         return view
     }()
     
+    let checkMark: UIImageView = {
+        let width: CGFloat = 40
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "LivenessCheckmark")
+        imageView.contentMode = .scaleAspectFit
+        imageView.snp.makeConstraints {
+            $0.size.equalTo(width)
+        }
+        imageView.alpha = 1
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -55,9 +67,16 @@ class ViewController: UIViewController {
 private extension ViewController {
     func setupUI() {
         view.backgroundColor = .white
+        
+        dot1.transform = CGAffineTransform(scaleX: 0, y: 0)
+        dot2.transform = CGAffineTransform(scaleX: 0, y: 0)
+        dot3.transform = CGAffineTransform(scaleX: 0, y: 0)
+        checkMark.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
         view.addSubview(dot1)
         view.addSubview(dot2)
         view.addSubview(dot3)
+        view.addSubview(checkMark)
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(animate))
         view.addGestureRecognizer(tapGestureRecognizer)
     }
@@ -74,22 +93,37 @@ private extension ViewController {
         dot3.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
+        
+        checkMark.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
     
     @objc func animate() {
         dot1.layer.add(makeAnimation(stiffness: 100, damping: 12), forKey: nil)
         dot2.layer.add(makeAnimation(stiffness: 200, damping: 10), forKey: nil)
         dot3.layer.add(makeAnimation(stiffness: 300, damping: 12), forKey: nil)
+        checkMark.layer.add(makeAnimation(stiffness: 50,
+                                          damping: 10,
+                                          duration: 0.05,
+                                          delay: 0.25), forKey: nil)
     }
     
-    func makeAnimation(stiffness: CGFloat, damping: CGFloat) -> CASpringAnimation {
+    func makeAnimation(stiffness: CGFloat,
+                       damping: CGFloat,
+                       duration: TimeInterval = 1,
+                       delay: TimeInterval = 0) -> CASpringAnimation {
         let animation = CASpringAnimation(keyPath: "transform.scale")
         animation.duration = 1
+        animation.beginTime = CACurrentMediaTime() + delay
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         animation.stiffness = stiffness
         animation.damping = damping
         animation.mass = 1
         animation.fromValue = 0
         animation.toValue = 1
+        animation.fillMode = .both
+        animation.isRemovedOnCompletion = false
         return animation
     }
 }
