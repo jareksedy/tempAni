ebe382994ea8ff91975650173cd363bd984c4a80
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-10-31 19:52:15 +0600

    Basic setup.

--- tempAni/ViewController.swift
+++ tempAni/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    let dot: UIView = {
+        let width: CGFloat = 150
+        let view = UIView()
+        view.backgroundColor = .systemMint
+        view.snp.makeConstraints {
+            $0.size.equalTo(width)
+        }
+        view.layer.cornerRadius = width / 2
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        makeConstraints()
     }
-
-
 }
 
+private extension ViewController {
+    func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(dot)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(animate))
+        dot.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func makeConstraints() {
+        dot.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    @objc func animate() {
+        self.dot.transform = CGAffineTransform(scaleX: 0, y: 0)
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.3,
+                       initialSpringVelocity: 0.14,
+                       options: [.allowUserInteraction]) {
+            self.dot.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+    }
+}
